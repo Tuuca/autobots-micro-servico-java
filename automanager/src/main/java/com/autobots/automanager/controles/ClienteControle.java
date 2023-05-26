eaf1a61af7ac3608762882cc0265c7cade69ac06
Gabriel <gabriel.macedo6@fatec.sp.gov.br>
2023-05-26 17:59:01 -0300

    Finalização CRUDs

--- automanager/src/main/java/com/autobots/automanager/controles/ClienteControle.java
+++ automanager/src/main/java/com/autobots/automanager/controles/ClienteControle.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.autobots.automanager.entidades.Cliente;
-import com.autobots.automanager.modelo.ClienteAtualizador;
-import com.autobots.automanager.modelo.ClienteSelecionador;
+import com.autobots.automanager.modelo.cliente.ClienteAtualizador;
+import com.autobots.automanager.modelo.cliente.ClienteSelecionador;
 import com.autobots.automanager.repositorios.ClienteRepositorio;
 
 @RestController
